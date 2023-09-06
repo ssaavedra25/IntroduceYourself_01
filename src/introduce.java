9253e0f4cb5509cbe71c91c8232c3ba396982a69
ssaavedra25 <stipanisaaveds@gmail.com>
2023-09-06 22:25:47 +0800

    2nd commit

--- src/introduce.java
+++ src/introduce.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class introduce {
-}
+    class introduce {
+        public static void main(String[] args) {
+            System.out.println("Hi! My name is Stephanie N. Saavedra! Hello again");
+        }
+    }
+
